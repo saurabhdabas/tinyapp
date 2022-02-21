a31fba0c7b573707044bed49b29dd9b062de88c2
saurabhdabas <developerdabas@gmail.com>
2022-02-21 23:29:09 +0000

    Added more paths

--- express_server.js
+++ express_server.js
@@ -1,3 +1,7 @@
+function generateRandomString() {
+  return Math.random().toString(30).substring(2,8);
+}
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -8,6 +12,9 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 // This line of code registers a handler on the root path, "/".
 app.get("/", (req, res) => {
@@ -27,10 +34,27 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index",templateVars)
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  // console.log(req.body);  // Log the POST request body to the console
+  // // res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let longURL = req.body.longURL ;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL]=longURL ;
+  res.redirect(`/urls/${shortURL}`)
+});
+// generateRandomString();
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
