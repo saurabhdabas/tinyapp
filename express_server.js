1fd897c79303ad5f2b1a2a22c71eeeb7bd35d8df
saurabhdabas <developerdabas@gmail.com>
2022-02-21 19:21:11 +0000

    created an HTTP Server

---
+++ express_server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// This line of code registers a handler on the root path, "/".
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
