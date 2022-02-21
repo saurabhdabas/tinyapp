b6bdba32fc4a20d1f7211cafb72cae9b636f7a13
saurabhdabas <developerdabas@gmail.com>
2022-02-21 20:54:31 +0000

    Added a new path

--- express_server.js
+++ express_server.js
@@ -19,9 +19,18 @@ app.get("/urls.json", (req, res) => {
 });
 // This line of code registers a handler on the  path, "/hello".
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index",templateVars)
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
