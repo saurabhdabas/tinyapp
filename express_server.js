e8e8cade61677f83cc7ec791115604faf6b57779
saurabhdabas <developerdabas@gmail.com>
2022-02-23 22:42:18 +0000

    Added a registration handler

--- express_server.js
+++ express_server.js
@@ -12,11 +12,24 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+}
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const req = require("express/lib/request");
 app.use(cookieParser());
 
 // This line of code registers a handler on the root path, "/".
@@ -29,29 +42,51 @@ app.get("/urls.json", (req, res) => {
 });
 app.post('/login', function (req, res) {
   res.cookie("name",req.body.username);
-  console.log(req);
+
   res.redirect("/urls")
+
 })
 app.post('/logout', function (req, res) {
   res.clearCookie("name");
-  console.log(req);
+
   res.redirect("/urls")
 })
+app.get("/register", (req, res) => {
+  res.render("user_registration");
+  console.log("req.cookies:",req.cookies);
+});
+const findAUser = function(users,user_id){
+  const userArray = Object.values(users);
+  const getAUser = userArray.filter((user)=>{
+    return  user.id === user_id
+  });
+  return getAUser[0]
+}
+app.post("/register", (req, res) => {
+  const randomID = generateRandomString();
+  users[randomID] = {    id: randomID, 
+  email: req.body.email, 
+  password: req.body.password }
+  res.cookie("user_id", users[randomID].id)
+  const user = findAUser(users,randomID);
+
+  res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase , username: req.cookies["name"] };
-  console.log(templateVars);
+  const templateVars = { urls: urlDatabase ,user :users[req.cookies.user_id]};
   res.render("urls_index",templateVars)
 });
 
 app.post("/urls", (req, res) => {
-  // console.log(req.body);  // Log the POST request body to the console
+
   let longURL = req.body.longURL ;
   const shortURL = generateRandomString();
   urlDatabase[shortURL]=longURL ;
   res.redirect(`/urls/${shortURL}`)
 });
 app.get("/urls/new", (req, res) => {
-  const templateVars = {username: req.cookies["name"] };
+  const templateVars = {username: req.cookies["name"] ,user :users[req.cookies.user_id]};
   console.log(templateVars);
   res.render("urls_new",templateVars);
 });
@@ -61,7 +96,7 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],username: req.cookies["name"]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],user :users[req.cookies.user_id]};
   res.render("urls_show", templateVars);
 });
 app.post("/urls/:id", (req, res) => {
